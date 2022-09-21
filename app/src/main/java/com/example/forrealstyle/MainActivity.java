24e68a8c30b9a7298a045ec725db3d825fb95ece
parikh6486 <112438759+anshparikh18@users.noreply.github.com>
2022-09-20 21:41:10 -0500

    final project
    done

--- app/src/main/java/com/example/forrealstyle/MainActivity.java
+++ app/src/main/java/com/example/forrealstyle/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.forrealstyle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button goToActivityTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToActivityTwo = findViewById((R.id.activity_main_goToActivityTwo));
+        goToActivityTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                changeActivity();
+
+            }
+        });
+    }
+    private void changeActivity(){
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent);
     }
 }
